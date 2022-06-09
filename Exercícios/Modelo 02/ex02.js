5721af66a0aaf9b2cf8fd99b60f0a73535e233e2
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2022-06-09 14:30:12 -0300

    New Updates

---
+++ Exercícios/Modelo 02/ex02.js
@@ -0,0 +1,50 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'images/bebe_homem.png')
+            }else if(idade <= 29){
+                //jovem
+                img.setAttribute('src', 'images/jovem_homem.png')
+            }else if(idade <= 50){
+                //adulto
+                img.setAttribute('src', 'images/adulto_homem.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'images/idoso_homem.png')
+            }
+        }else{
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'images/bebe_mulher.png')
+            }else if(idade <= 29){
+                //jovem
+                img.setAttribute('src', 'images/jovem_mulher.png')
+            }else if(idade <= 50){
+                //adulto
+                img.setAttribute('src', 'images/adulto_mulher.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'images/idoso_mulher.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
